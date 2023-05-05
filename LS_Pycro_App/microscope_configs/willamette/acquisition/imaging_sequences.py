e0147797585568c6cf5fb7fb748c1e0c4b7b9163
Jonah <rpbiolab@gmail.com>
2023-05-05 12:44:55 -0700

    PcoEdge changed to Camera to reflect name change

--- LS_Pycro_App/microscope_configs/willamette/acquisition/imaging_sequences.py
+++ LS_Pycro_App/microscope_configs/willamette/acquisition/imaging_sequences.py
@@ -1,34 +1,33 @@
 from acquisition.imaging_sequences import Snap, Video, SpectralVideo, ZStack, SpectralZStack
-from microscope_configs.willamette.hardware.camera import PcoEdge
-from microscope_configs.willamette.hardware.plc import WilPlc
+from microscope_configs.willamette.hardware.camera import Camera
 from utils.pycro import core
 
 class Snap(Snap):
     def _pre_acquire_hardware_init(self):
         core.set_exposure(self._region.snap_exposure)
-        PcoEdge.set_burst_mode()
+        Camera.set_burst_mode()
 
 
 class Video(Video):
     def _pre_acquire_hardware_init(self):
         core.set_exposure(self._region.video_exposure)
-        PcoEdge.set_burst_mode()
+        Camera.set_burst_mode()
 
 
 class SpectralVideo(SpectralVideo):
     def _pre_acquire_hardware_init(self):
         core.set_exposure(self._region.video_exposure)
-        PcoEdge.set_burst_mode()
+        Camera.set_burst_mode()
 
 
 class ZStack(ZStack):
     def _pre_acquire_hardware_init(self):
         core.set_exposure(self._adv_settings.z_stack_exposure)
-        PcoEdge.set_ext_trig_mode()
+        Camera.set_ext_trig_mode()
 
 
 class SpectralZStack(SpectralZStack):
     def _pre_acquire_hardware_init(self):
         core.set_exposure(self._adv_settings.z_stack_exposure)
-        PcoEdge.set_burst_mode()
+        Camera.set_burst_mode()
 
