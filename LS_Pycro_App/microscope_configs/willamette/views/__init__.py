a58bb12115fc8eb3127ea0a1202824f59ba78536
Jonah <rpbiolab@gmail.com>
2023-05-10 16:43:57 -0700

    Correctly uses willamette main window

--- LS_Pycro_App/microscope_configs/willamette/views/__init__.py
+++ LS_Pycro_App/microscope_configs/willamette/views/__init__.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets
-from microscope_configs.klamath.views.py.adv_settings_dialog import Ui_AdvSettingsDialog
-from microscope_configs.klamath.views.py.main_window import Ui_MainWindow
+from microscope_configs.willamette.views.py.adv_settings_dialog import Ui_AdvSettingsDialog
+from microscope_configs.willamette.views.py.main_window import Ui_MainWindow
 
 class AdvSettingsDialog(QtWidgets.QDialog, Ui_AdvSettingsDialog):
     def __init__(self):
