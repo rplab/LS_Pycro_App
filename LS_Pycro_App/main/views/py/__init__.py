9b6259f441c881bd7284c8a1e9da5a9f4a378aa5
Jonah <jonahs@uoregon.edu>
2023-11-21 19:13:50 -0800

    main window split into two files

--- LS_Pycro_App/main/views/py/__init__.py
+++ LS_Pycro_App/main/views/py/__init__.py
@@ -2,9 +2,9 @@ from PyQt5 import QtWidgets
 from LS_Pycro_App.main.microscope_select.microscope_select import microscope, MicroscopeConfig
 
 if microscope == MicroscopeConfig.WILLAMETTE:
-    from LS_Pycro_App.main.views.py.main_window import Wil_Ui_MainWindow as Ui_MainWindow
+    from LS_Pycro_App.main.views.py.wil_main_window import Ui_WilMainWindow as Ui_MainWindow
 elif microscope == MicroscopeConfig.KLAMATH:
-    from LS_Pycro_App.main.views.py.main_window import Kla_Ui_MainWindow as Ui_MainWindow
+    from LS_Pycro_App.main.views.py.kla_main_window import Ui_KlaMainWindow as Ui_MainWindow
 
 
 class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
