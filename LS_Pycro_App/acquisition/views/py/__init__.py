80ac1031859495df45a88234c4dafa93e29db405
Jonah <rpbiolab@gmail.com>
2024-01-12 12:04:38 -0800

    adv_settings_dialog no longer split between KLA and WIL

--- LS_Pycro_App/acquisition/views/py/__init__.py
+++ LS_Pycro_App/acquisition/views/py/__init__.py
@@ -1,15 +1,10 @@
 from PyQt5 import QtWidgets
-from LS_Pycro_App.main.microscope_select.microscope_select import microscope, MicroscopeConfig
 from LS_Pycro_App.acquisition.views.py.abort_dialog import Ui_AbortDialog
 from LS_Pycro_App.acquisition.views.py.acq_dialog import Ui_AcqDialog
 from LS_Pycro_App.acquisition.views.py.acq_order_dialog import Ui_AcqOrderDialog
 from LS_Pycro_App.acquisition.views.py.acq_regions_dialog import Ui_AcqRegionsDialog
 from LS_Pycro_App.acquisition.views.py.acq_settings_dialog import Ui_AcqSettingsDialog
-
-if microscope == MicroscopeConfig.WILLAMETTE:
-    from LS_Pycro_App.acquisition.views.py.adv_settings_dialog import Wil_Ui_AdvSettingsDialog as Ui_AdvSettingsDialog
-elif microscope == MicroscopeConfig.KLAMATH:
-    from LS_Pycro_App.acquisition.views.py.adv_settings_dialog import Kla_Ui_AdvSettingsDialog as Ui_AdvSettingsDialog
+from LS_Pycro_App.acquisition.views.py.adv_settings_dialog import Ui_AdvSettingsDialog
 
 
 class AbortDialog(QtWidgets.QDialog, Ui_AbortDialog):
