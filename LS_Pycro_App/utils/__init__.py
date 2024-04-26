975d7362d18596026f2d48f12c072353f693e746
jsokoloff23 <jonahs@uoregon.edu>
2024-04-26 12:06:41 -0700

    added separate user config file creation for HTLS

--- LS_Pycro_App/utils/__init__.py
+++ LS_Pycro_App/utils/__init__.py
@@ -1,5 +1,9 @@
+from LS_Pycro_App.controllers.select_controller import microscope, MicroscopeConfig
 from LS_Pycro_App.utils.init_logger import init_logger
 from LS_Pycro_App.utils.config import Config
 
 init_logger()
-user_config = Config(filename="LSConfig.cfg")
+if microscope == MicroscopeConfig.HTLS:
+    user_config = Config(filename="HTLSConfig.cfg")
+else:
+    user_config = Config(filename="LSConfig.cfg")
